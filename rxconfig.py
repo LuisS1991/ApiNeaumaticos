89057a85c30935cb072d67376ee76726414b37eb
Luis <112443531+LuisS1991@users.noreply.github.com>
2024-10-18 17:51:53 -0300

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -3,10 +3,9 @@ import reflex as rx
 config = rx.Config(
     app_name="ApiNeaumaticos", 
     db_url="sqlite:///reflexUser.db",
-    api_url="https://apineumaticos-production.up.railway.app" 
 )
 """
-"http://app.example.com:8000",
+    api_url="https://apineumaticos-production.up.railway.app" 
 cors_allowed_origins=[
     "https://docs-api-neumaticos.vercel.app/"
     "*"
