5e6e63557d94292cb5970105ca1c302864dff032
LuisS1991 <nantesluis72@gmail.com>
2024-10-18 17:38:16 -0300

    backurl

--- rxconfig.py
+++ rxconfig.py
@@ -2,9 +2,11 @@ import reflex as rx
 
 config = rx.Config(
     app_name="ApiNeaumaticos", 
-      db_url="sqlite:///reflexUser.db"
+    db_url="sqlite:///reflexUser.db",
+    api_url="https://apineumaticos-production.up.railway.app" 
 )
 """
+"http://app.example.com:8000",
 cors_allowed_origins=[
     "https://docs-api-neumaticos.vercel.app/"
     "*"
