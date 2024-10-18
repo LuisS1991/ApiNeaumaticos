86ebf6b4fc2ddc9aa0513d2cd792ad840139192e
LuisS1991 <nantesluis72@gmail.com>
2024-10-18 17:49:44 -0300

    backurl ok

--- export.sh
+++ export.sh
@@ -2,3 +2,8 @@ API_URL=https://apineumaticos-production.up.railway.app reflex export --frontend
 unzip frontend.zip -d public
 rm -f frontend.zip
 chmod -R 777 ./public 
+
+
+#win 10
+#$env:API_URL="https://apineumaticos-production.up.railway.app"
+#reflex export --frontend-only
